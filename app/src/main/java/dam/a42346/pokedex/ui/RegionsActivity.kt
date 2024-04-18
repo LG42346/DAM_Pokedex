3747582ac3648d0aabcbadbf0c60bc2d82165805
Anon <alexguimaraes95@gmail.com>
2024-04-18 15:59:50 +0100

    2nd challenge

--- app/src/main/java/dam/a42346/pokedex/ui/RegionsActivity.kt
+++ app/src/main/java/dam/a42346/pokedex/ui/RegionsActivity.kt
@@ -1,11 +1,16 @@
 package dam.a42346.pokedex.ui
 
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import dam.a42346.pokedex.R
+import dam.a42346.pokedex.model.mocks.MockData
 
 class RegionsActivity : BottomNavActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var listView = findViewById<RecyclerView>(R.id.regionsRecyclerView)
+        listView.adapter = RegionAdapter(pkRegionList = MockData.regions, context = this)
     }
 
     override val contentViewId: Int
